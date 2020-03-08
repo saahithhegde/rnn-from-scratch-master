ae2c9f86b93d2e1ce5de2fd099e9644fbcda5487
saahithhegde <saahithhegde@yahoo.co.in>
2020-03-08 14:19:33 -0500

    Initial commit

---
+++ generate.py
@@ -0,0 +1,11 @@
+from rnn import RNN
+from tokens import tokenize, detokenize
+
+model = RNN(61,200)
+model.load('uwv.pkl')
+print(model.U.shape)
+# generate(start_token, num_of_chars, top_k random predictions)
+text = model.generate(tokenize('A'), 300, 3)
+
+for i in text:
+    print(detokenize(i), end="")
